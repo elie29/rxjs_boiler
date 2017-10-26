21f0058c5a21889f9d68e53334a7ca2890684d5b
Elie NEHME <elie29@gmail.com>
2017-10-26 17:49:29 +0000

    update dependencies

--- src/app.js
+++ src/app.js
@@ -2,3 +2,11 @@ import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
 console.log('RxJS Boiler Running...');
+
+const btn = $('#btn');
+
+// btn.click becomes an observable
+const btnStream = Rx.Observable.fromEvent(btn, 'click');
+// to the observable, we subscribe a log observer
+btnStream.subscribe((e) => console.log(e));
+
