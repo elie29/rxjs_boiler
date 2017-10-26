9e36442031847d48e247b20fcd16d6f3fafada14
Elie NEHME <elie29@gmail.com>
2017-10-26 22:01:05 +0200

    Add Observable

--- src/app.js
+++ src/app.js
@@ -4,9 +4,46 @@ import Rx from 'rxjs/Rx';
 console.log('RxJS Boiler Running...');
 
 const btn = $('#btn');
+const output = $('#div');
 
 // btn.click becomes an observable
 const btnStream = Rx.Observable.fromEvent(btn, 'click');
 // to the observable, we subscribe a log observer
 btnStream.subscribe((e) => console.log(e));
 
+// Same for mousemove
+const mouseMove = Rx.Observable.fromEvent(document, 'mousemove');
+mouseMove.subscribe((e) => output.html('<h1>X: ' + e.clientX + ', Y: ' + e.clientY));
+
+// Create Observable from Array
+const numbers = [33, 44, 55, 66];
+Rx.Observable.from(numbers).subscribe(
+	v => console.log(v),
+	error => console.log(error),
+	() => console.log('completed 1') // Array stream is limited so completed will be fired !!
+);
+
+const map = new Map([[1,'1'], [2,'2'], [3,'3']]);
+Rx.Observable.from(map).subscribe(
+	v => console.log(v),
+	error => console.log(error),
+	() => console.log('completed 2') // Map stream is limited so completed will be fired !!
+);
+
+// From scratch
+new Rx.Observable(subject => {
+	subject.next('Creating Observable');
+	subject.next('Hello world');
+	timeOut(subject);
+}).subscribe(
+	x => console.log(x),
+	error => console.log(error),
+	complete => console.log('completed 3')
+);
+
+function timeOut(subject) {
+    setTimeout(() => {
+        subject.next('Yet another value!');
+        subject.complete(); // To end the stream
+    }, 3000);
+}
